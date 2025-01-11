eccc4dccd9b0cf0c88e2b673e69188c3269b478f
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2025-01-12 00:39:45 +0900

    2025-01-11

---
+++ java_src/B5_10171_고양이.java
@@ -0,0 +1,10 @@
+package java_src;
+
+public class B5_10171_고양이 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
