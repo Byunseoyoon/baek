9a29b2a5cb7858a35f0f56bd11c015d3356f6e97
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2025-02-15 23:55:27 +0900

    2025-02-15

---
+++ java_src/B5_2743_단어길이재기.java
@@ -0,0 +1,11 @@
+package java_src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_2743_단어길이재기 {
+    public static void main(String[] args) throws IOException{
+        System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().length());
+    }
+}
