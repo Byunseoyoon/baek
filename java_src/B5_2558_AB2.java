d03de76b6bd96c7b5a08a1fa767953ab6a8c3319
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2025-08-13 00:53:54 +0900

    2025-08-12

---
+++ java_src/B5_2558_AB2.java
@@ -0,0 +1,15 @@
+package java_src;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class B5_2558_AB2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()) + "");
+        bw.flush();
+    }
+}
