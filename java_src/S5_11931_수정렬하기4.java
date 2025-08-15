1b32a4551eca49d3bf7619a7cd38a387110c759e
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2025-08-16 05:41:50 +0900

    2025-08-15

---
+++ java_src/S5_11931_수정렬하기4.java
@@ -0,0 +1,24 @@
+package java_src;
+
+import java.io.*;
+import java.util.*;
+
+public class S5_11931_수정렬하기4 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i = arr.length-1; i >= 0; i--) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+
+    }
+}
